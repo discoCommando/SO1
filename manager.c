a710c16d2b19a95926f6b1999eb823156c3812ae
Tomasz Wawreniuk <tw336034@khaki08.mimuw.edu.pl>
2013-11-18 19:51:28 +0100

    Poprawiono manager.c dodano funkcje tworzPotomka

--- manager.c
+++ manager.c
@@ -5,9 +5,27 @@
 #include <sys/wait.h>
 #include "err.h"
 
-void tworzPotomka()
+unsigned int licznik = 0;
+unsigned int iloscWykonawcow = 0;
+void tworzPotomka(int deskryptorOdczytuPotomka)
 {
+	if (licznik < iloscWykonawcow)
+	{
+		int pipe_nast[2];
+		if (close (0) == -1)            syserr("child, close (0)");
+		if (dup (deskryptorOdczytuPotomka) != 0)    syserr("child, dup (deskryptorOdczytuPotomka)");
+		if (close (deskryptorOdczytuPotomka) == -1) syserr("child, close (deskryptorOdczytuPotomka)])");
+		
+		if (pipe (pipe_nast) == -1) syserr("Error in pipe\n");
+		
+		
+		if (close (1) == -1)            syserr("child, close (0)");
+		if (dup (pipe_nast[1]) != 0)    syserr("child, dup (pipe_nast[1])");
+		if (close (pipe_nast[1]) == -1) syserr("child, close (pipe_nast[1])])");
 	
+		++licznik;
+		tworzPotomka(pipe_nast[1]);
+	}
 }
 
 int main(int argc, char* argv[])
@@ -18,7 +36,7 @@ int main(int argc, char* argv[])
 	}
 	
 	int pipe_dsc [2];
-	int iloscWykonawcow = atoi(argv[1]);
+	iloscWykonawcow = atoi(argv[1]);
 	
 	if (pipe (pipe_dsc) == -1) syserr("Error in pipe\n");
 	
